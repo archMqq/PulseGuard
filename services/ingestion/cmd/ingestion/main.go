eab7fce2b86919008f84edc26d8966a14b79b7c8
Stepan Ryzhkov <clouztoyou@mail.ru>
2026-06-22 22:08:00 +0300

    project main fill + http changes

--- services/ingestion/cmd/ingestion/main.go
+++ services/ingestion/cmd/ingestion/main.go
@@ -1,1 +1,65 @@
 package main
+
+import (
+	"context"
+	"errors"
+	"log"
+	nethttp "net/http"
+	"os"
+	"os/signal"
+	"pulseguard/services/ingestion/internal/cache"
+	"pulseguard/services/ingestion/internal/config"
+	"pulseguard/services/ingestion/internal/http"
+	"pulseguard/services/ingestion/internal/queue"
+	"pulseguard/services/ingestion/internal/service"
+	"sync"
+	"syscall"
+	"time"
+)
+
+func main() {
+	cfg, err := config.ReadConfig("../../internal/config", "yaml")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	redis, err := cache.NewRedisProjectCache(context.Background(), cfg.Redis)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	injectService := service.NewErrInjectionService(redis)
+	kafka := queue.NewKafkaQueue(cfg.Kafka)
+
+	shutdownWg := &sync.WaitGroup{}
+
+	handler := http.NewHttpHandler(&injectService, kafka, shutdownWg)
+	server := &nethttp.Server{
+		Addr:    cfg.Addr,
+		Handler: handler,
+	}
+
+	shutdownChan := make(chan struct{})
+
+	go func() {
+		sigchan := make(chan os.Signal, 1)
+		signal.Notify(sigchan, os.Interrupt, syscall.SIGTERM)
+		<-sigchan
+
+		ctx, cancel := context.WithTimeout(context.Background(), 30*time.Second)
+		defer cancel()
+
+		if err := server.Shutdown(ctx); err != nil {
+			log.Printf("server shutdown error: %v", err)
+		}
+
+		shutdownWg.Wait()
+		close(shutdownChan)
+	}()
+
+	if err := server.ListenAndServe(); err != nil && !errors.Is(err, nethttp.ErrServerClosed) {
+		log.Fatalf("server error: %v: ", err)
+	}
+
+	<-shutdownChan
+}
--- services/ingestion/internal/http/http.go
+++ services/ingestion/internal/http/http.go
@@ -1,11 +1,15 @@
 package http
 
 import (
+	"context"
 	"encoding/json"
+	"log"
 	"net/http"
 	"pulseguard/services/ingestion/internal/queue"
 	"pulseguard/services/ingestion/internal/service"
 	"pulseguard/services/pkg/contracts"
+	"sync"
+	"time"
 
 	"github.com/gin-gonic/gin"
 )
@@ -14,14 +18,16 @@ type HttpHandler struct {
 	injectionService service.InjectionService
 	queue            queue.QueueSaver
 	router           *gin.Engine
+	wg               *sync.WaitGroup
 }
 
-func NewHttpHandler(injectService service.InjectionService, queue queue.QueueSaver) *HttpHandler {
+func NewHttpHandler(injectService service.InjectionService, queue queue.QueueSaver, wg *sync.WaitGroup) *HttpHandler {
 	newRouter := gin.New()
 	handler := &HttpHandler{
 		injectionService: injectService,
 		queue:            queue,
 		router:           newRouter,
+		wg:               wg,
 	}
 
 	newRouter.POST("/error", handler.postError)
@@ -43,12 +49,14 @@ func (hh *HttpHandler) postError(c *gin.Context) {
 		c.IndentedJSON(http.StatusBadRequest, gin.H{
 			"message": "invalid request",
 		})
+		return
 	}
 
 	if err := hh.injectionService.ValidateErrorEvent(c.Request.Context(), &event, pk); err != nil {
 		c.IndentedJSON(http.StatusUnauthorized, gin.H{
 			"message": "project unauthorized",
 		})
+		return
 	}
 
 	eventJson, err := json.Marshal(event)
@@ -56,13 +64,24 @@ func (hh *HttpHandler) postError(c *gin.Context) {
 		c.IndentedJSON(http.StatusInternalServerError, gin.H{
 			"message": "error event marshaling",
 		})
+		return
 	}
 
-	if err := hh.queue.Save(c, string(eventJson)); err != nil {
-		c.IndentedJSON(http.StatusInternalServerError, gin.H{
-			"message": err.Error(),
-		})
-	}
+	hh.wg.Add(1)
+	go func(event string) {
+		defer hh.wg.Done()
+
+		ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+		defer cancel()
+
+		if err := hh.queue.Save(ctx, event); err != nil {
+			log.Printf("error sanding to kafka")
+		}
+	}(string(eventJson))
 
 	c.IndentedJSON(http.StatusAccepted, gin.H{})
+}
+
+func (hh *HttpHandler) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+	hh.router.ServeHTTP(w, req)
 }
