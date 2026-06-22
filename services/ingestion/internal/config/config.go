7ad39cf15fcc5b335b8239a3fffa3a056dae89cf
Stepan Ryzhkov <clouztoyou@mail.ru>
2026-06-22 22:07:03 +0300

    yaml config add + fixes in config.go

--- services/ingestion/internal/config/config.go
+++ services/ingestion/internal/config/config.go
@@ -1,17 +1,45 @@
 package config
 
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
 type Config struct {
-	RedisConfig
+	Addr  string      `mapstructure:"addr"`
+	Kafka KafkaConfig `mapstructure:"kafka"`
+	Redis RedisConfig `mapstructure:"redis"`
 }
 
 type RedisConfig struct {
-	Addr     string
-	Pass     string
-	DB       int
-	PoolSize int
+	Addr     string `mapstructure:"addr"`
+	Pass     string `mapstructure:"pass"`
+	DB       int    `mapstructure:"db"`
+	PoolSize int    `mapstructure:"pool_size"`
 }
 
 type KafkaConfig struct {
-	Addr  []string
-	Topic string
+	Addr  []string `mapstructure:"addr"`
+	Topic string   `mapstructure:"topic"`
+}
+
+func ReadConfig(path string, typ string) (*Config, error) {
+	viper.SetConfigName("config")
+	viper.AddConfigPath(path)
+	viper.AutomaticEnv()
+	viper.SetConfigType(typ)
+
+	var cfg Config
+
+	if err := viper.ReadInConfig(); err != nil {
+		return nil, fmt.Errorf("config read error: %w", err)
+	}
+
+	err := viper.Unmarshal(&cfg)
+	if err != nil {
+		return nil, fmt.Errorf("unable to decode to config struct, error: %w", err)
+	}
+
+	return &cfg, nil
 }
